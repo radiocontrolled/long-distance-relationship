22a957f5bc44030ad7f77d44aa6df6da42d54c32
radiocontrolled <radiocontrolled@gmail.com>
2014-12-07 11:58:33 +0000

    add Saturn rings, clean in jshint

--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@
 * Demo: <a href="http://radiocontrolled.github.io/planets/index.html">http://radiocontrolled.github.io/planets/index.html</a>
 * Distance, but not sizes, are to scale
 * Source: <a href="http://en.wikipedia.org/wiki/Solar_System">Solar System, wikipedia</a>
+
--- vis.js
+++ vis.js
@@ -15,10 +15,21 @@ var getViewportDimensions = function(){
 getViewportDimensions();
 
 d3.json("planets.json", function(error, data) {
-	
-  planetaryData = data; 
+
+  if(data){
+  	
+ 	 planetaryData = data; 
+ 	 
+ 	 visualise(planetaryData, height); 	
   
-  visualise(planetaryData, height);
+  }
+  
+  else if(error){
+  	
+  	console.log(error);
+  
+  }
+ 
 
 });
 
@@ -39,14 +50,20 @@ var visualise = function(planetaryData, height){
 	
 	// hold the range of distances to the sun
 	var distances = [],
-		radiuses = [];
+		radiuses = [], 
+		saturn;
 		
 	for(var key in planetaryData){
 		if(planetaryData.hasOwnProperty(key)){
 			distances.push(planetaryData[key]["Mean distance from Sun (AU)"]);
-			radiuses.push(planetaryData[key]["Equatorial radius (KM)"]);	
+			radiuses.push(planetaryData[key]["Equatorial radius (KM)"]);
+			if(planetaryData[key].Planet == "Saturn"){
+				saturn = planetaryData[key];
+			}
 		}
 	}
+		
+			
 		
 	var distanceFromSunScale = d3.scale.linear()
 		.domain([d3.min(distances),d3.max(distances)]) //input domain = min & max distances
@@ -72,36 +89,58 @@ var visualise = function(planetaryData, height){
 			"cx": function(){
 				return width/2;
 			},
-			"fill": "#ecf0f1"
+			"fill": "#ecf0f1",
+			"id": function(d){
+				return d.Planet;
+			}
 			
 		});
+		
+	
 	
 	var labels = solarSystem.selectAll("text")
 		.data(planetaryData)
 		.enter()
-		
 		.append("text")
 		.text(function(d){
-			return d["Planet"] + ", " + d["Mean distance from Sun (AU)"] + " au";
+			return d.Planet + ", " + d["Mean distance from Sun (AU)"] + " au";
 		})
 		.attr({
 			"x": function(){
-				return (width/2) + 30;
+				return (width/2) + 50;
 			},
 			"y": function(d){
-				return  distanceFromSunScale(d["Mean distance from Sun (AU)"] );
+				return distanceFromSunScale(d["Mean distance from Sun (AU)"] );
 			},
 			"fill": "white",
 			"text-anchor": "start"
 		});
 
-	
 
+	/* add rings to saturn - use a rect with rounded corners to represent this*/
+	var saturnRings = d3.select("svg")
+		.append("rect")
+		.attr({
+			"x": function(){
+				return (width/2) - ((radiusScale(saturn["Equatorial radius (KM)"]) * 2  )   );
+			},
+			"y": function(){
+				return distanceFromSunScale(saturn["Mean distance from Sun (AU)"]);
+			},
+			"rx":20, 
+			"ry":20,
+			"fill":"#ecf0f1",
+			"height":5,
+			"width": function(){
+				 return radiusScale(saturn["Equatorial radius (KM)"]) * 4 ;
+			}
+			
+		});	
 };
 
 d3.select(window).on('resize', resize);
 
-function resize(globe) {
+function resize() {
 	getViewportDimensions();
 	
 }
